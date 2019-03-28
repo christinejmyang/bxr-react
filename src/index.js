95debd57d39a3fc3ba31ac9af0d9eda8043811a2
Daniel Berlin <berlindan8@gmail.com>
2019-03-28 12:12:36 -0400

    added firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import './index.css';
 import App from './App';
 import About from './Components/about.js';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC5HjoTU7oDrhoogx70m-na8WvcpinFL_E", // replace with .env file's info when pushing to git -- don't want apiKey publicly visible
+  authDomain: "cs290-bxr.firebaseapp.com",
+  databaseURL: "https://cs290-bxr.firebaseio.com",
+  projectId: "cs290-bxr",
+  storageBucket: "cs290-bxr.appspot.com",
+  messagingSenderId: "889702854041"
+};
+
+firebase.initializeApp(config);
+
+// Initialize the default app
+/* var defaultApp = firebase.initializeApp(defaultAppConfig);
+console.log(defaultApp.name);  // "[DEFAULT]"
+defaultStorage = firebase.storage();
+defaultDatabase = firebase.database(); */
 
 ReactDOM.render(<App />,
 document.getElementById('root'));
