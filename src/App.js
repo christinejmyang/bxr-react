cf8166122a08caa1789564fc2c6dd1e197139028
Daniel Berlin <berlindan8@gmail.com>
2019-04-16 11:32:01 -0400

    sign in and sign up components added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import MainPic from './Components/mainpic.js';
-import HowItWorks from './Components/hiw.js';
-import About from './Components/about.js';
-import Footer from './Components/footer.jsx';
-import Products from './Components/products.jsx';
-import SignIn from './Components/signin.js';
-import Nav from './Components/nav.js';
-import firebase from './index.js';
+import React, { Component } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import MainPic from './Components/mainpic.js'
+import HowItWorks from './Components/hiw.js'
+import About from './Components/about.js'
+import Footer from './Components/footer.jsx'
+import Products from './Components/products.jsx'
+import SignIn from './Components/signin.js'
+import SignUp from './Components/signup.js'
+import Nav from './Components/nav.js'
+import firebase, {auth, user} from './firebase.js'
 
 class App extends Component {
-
-
   render() {
     const user = firebase.auth().currentUser;
 
-    const desktopHeader = (
+    return (
       <Router>
         <div className="App">
           <Nav />
@@ -27,13 +26,13 @@ class App extends Component {
             <Route exact path="/" component={HowItWorks} />
             <Route path="/about" component={About} />
             <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
           <Route component={Footer}></Route>
         </div>
       </Router>
     );
-
-  } //render
-} //extends
+  }
+}
 
 export default App;
--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,73 +1,24 @@
-import React, { Component } from 'react';
-import './../App.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Media from 'react-media';
-import firebase, {auth, provider} from './../index.js';
+import React, { Component } from 'react'
+import './../App.css'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Media from 'react-media'
+import firebase, {auth, provider} from './../firebase.js'
 import { Section, bodyTextStyle } from './Section.js'
+import SignedInLinks from './signedInLinks.js'
+import SignedOutLinks from './signedOutLinks.js'
+
 
 class Nav extends Component {
   render() {
-    const desktopHeader = (
+    return (
       <div>
         <header>
           <Link to="/"> <p class="header-index">bxr</p> </Link>
-          <nav>
-            <ul class="HeaderUl">
-              <li class="HeaderLink"><p>
-              <div class="dropdown"><a>Benefits</a><div class="dropdown-content">
-                  <Link to="/">For Brands</Link>
-                  <Link to="/">For Owners</Link>
-                  <Link to="/">For Renters</Link>
-                </div>
-              </div></p></li>
-              <li class="HeaderLink"> <Link to="/about"> <p> About </p> </Link> </li>
-              <li class="HeaderLink"> <Link to="/"> <p> How it Works </p> </Link> </li>
-              <li class="HeaderLink"> <Link to="/signin"> <p> Sign In </p> </Link> </li>
-            </ul>
-          </nav>
+          <SignedOutLinks />
         </header>
       </div>
     );
-
-    const mobileHeader = (
-      <div>
-          <header>
-            <Link to="/"> <p class="header-index">bxr</p> </Link>
-            <nav>
-              <ul class="HeaderUl">
-                <li class="HeaderLinkBurger">
-                  <p>
-                    <div class="hamburger">
-                      <a> &#9776; </a>
-                      <div class="dropdown-content">
-                        <Link to="/about">About</Link>
-                        <Link to="/">How It Works</Link>
-                        <Link to="/signin">Sign In</Link>
-                        <div class="dropdown2">
-                          <a>Benefits</a>
-                          <div class="dropdown-content2">
-                            <Link to="/">For Brands</Link>
-                            <Link to="/">For Owners</Link>
-                            <Link to="/">For Renters</Link>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </p>
-                </li>
-              </ul>
-            </nav>
-          </header>
-        </div>
-    );
-
-    return (
-        <Media query={{ minWidth: 500 }}>
-          {matches => (matches ? desktopHeader : mobileHeader)}
-        </Media>
-    );
   }
-
 }
 
 export default Nav;
