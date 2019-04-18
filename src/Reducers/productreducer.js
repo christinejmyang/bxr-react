596a02a7263dc588c2247ecb3809484ff0e52961
Daniel Berlin <berlindan8@gmail.com>
2019-04-17 23:03:18 -0400

    incorporating redux and thunk

--- src/Reducers/productreducer.js
+++ src/Reducers/productreducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  products: [
+    {id: 'item-1', name: 'Kendama', price: '20'},
+    {id: 'item-2', name: 'Wall Street Journal', price: '2'},
+    {id: 'item-3', name: 'TI-83 Plus C', price: '75'}
+  ]
+}
 
 const productReducer = (state = initState, action) => {
   return state;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import './index.css'
 import App from './App'
 import About from './Components/about.js'
 import * as serviceWorker from './serviceWorker'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './Reducers/rootreducer.js'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}> <App /> </Provider>,
 document.getElementById('root'));
