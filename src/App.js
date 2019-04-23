8ec2445cef7c7766b40866ce734bcef4439b5bc9
Daniel Berlin <berlindan8@gmail.com>
2019-04-23 11:21:57 -0400

    products page firebase fixed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import MainPic from './Components/mainpic.js'
 import HowItWorks from './Components/hiw.js'
 import About from './Components/about.js'
 import Footer from './Components/footer.jsx'
-//import Products from './Components/products.jsx'
+import Products from './Components/products.jsx'
 import SignIn from './Components/signin.js'
 import SignUp from './Components/signup.js'
 import Profile from './Components/profile.jsx'
 import Nav from './Components/nav.js'
 import Hosts from './Components/hosts.js'
 import Brands from './Components/brands.js'
+import Survey from './Components/survey.js'
 
 class App extends Component {
   render() {
@@ -25,12 +26,13 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={HowItWorks} />
             <Route path="/about" component={About} />
-            <Route path="/products" component={About} />
+            <Route path="/products" component={Products} />
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
             <Route path="/profile" component={Profile} />
             <Route path="/hosts" component={Hosts} />
             <Route path="/brands" component={Brands} />
+            <Route path="/survey" component={Survey} />
           </Switch>
           <Switch>
             <Route path="/" component={Footer} />
